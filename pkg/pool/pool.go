50470f813bb033f3b4fd1ac26a84c3f602e03998
Samarth S <samarthz0901@gmail.com>
2026-07-18 01:29:47 +0530

    17th

--- pkg/pool/pool.go
+++ pkg/pool/pool.go
@@ -1,22 +1,77 @@
 package pool
 
 import (
- "context"
- "net"
- "fmt"
- "sync"
+	"fmt"
+	"net"
+	"sync"
 )
 
-type ConnPool struct {
-	netConn net.Conn
-	mu       sync.Mutex
-	cond     *sync.Cond
+type PooledConn struct {
+	netConn   net.Conn
 	available bool
 }
 
 type Pool struct {
-	chan *ConnPool
-	maxSize int,
-	addr string,
+	idle    chan *PooledConn
+	maxSize int
+
+	addr    string
+}
+
+func NewPool(maxSize int, addr string) *Pool {
+
+	poolInst := &Pool{
+		idle:    make(chan *PooledConn, maxSize),
+		maxSize: maxSize,
+		addr:    addr,
+	}
+
+	return poolInst
+}
+func (p *Pool) dialNew()(*PooledConn, error) {
+	netConn,err:= net.Dial("tcp",p.addr)
+
+	if err != nil {
+		fmt.Println(" Failed to connect to server", err)
+		return nil,err
+	}
+
+	return &PooledConn {
+		netConn:netConn
+	},nil
+}
+func (p *Pool) Get()(*PooledConn, error) {
+select {
+case conn := <-p.idle:
+	if conn.available{
+		conn.available=false
+		return conn,nil
+	}
+
+default:
+	
+	rawConn, err := net.Dial("tcp",p.addr)
+	if err!= nil {
+		fmt.Println(" Failed to connect to server", err)
+		return nil,err
+	}
+return &PooledConn{netConn: rawConn}, nil 
+}
+}
+
+func (p *Pool) Put()(conn *PooledConn)error {
+
+	conn.available = true	
+	select {
+		case p.idle <- conn:
+			return nil
+		default:
+			conn.netConn.Close()
+			return fmt.Errorf("pool is full")
+	}
+
+}
+
+func (p *Pool) Close() error {
 
 }
